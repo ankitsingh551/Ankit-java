791ae14984db9d3a4639d649aefd7cb5ee590304
ankitsingh551 <kumarankit207020@gmail.com>
2024-03-28 08:13:03 +0530

    swap to no

---
+++ SwapTwo_no.java
@@ -0,0 +1,18 @@
+
+import java. util.Scanner;
+class SwapTwo_no
+{
+public static void main(String[] args)
+{
+ Scanner s=new Scanner(System.in);
+ System.out.println("Enter value for a");
+ int a=s.nextInt();
+ System.out.println("enter value for b");
+ int b=s.nextInt();
+ int t=a;
+ a=b;
+ b=t;
+ System.out.println("a="+a);
+ System.out.println("b="+b);
+}
+}
